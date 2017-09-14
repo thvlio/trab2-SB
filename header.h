880cc70c1142550a01d2a2f899800e25d3bff5c0
thvlio <thulionoslen_@hotmail.com>
2017-09-14 19:36:40 -0300

    Adicionadas algumas verificações de erro e modularização

--- header.h
+++ header.h
@@ -4,3 +4,132 @@
 #include <fstream> // std::ifstream
 #include <string> // std::string
 #include <sstream> // std::stringstream
+#include <algorithm> // ::toupper
+
+
+
+/*      CABEÇALHOS      */
+
+int errorCheck (int, char**);
+std::string o2pre (std::string);
+std::string o2mcr (std::string);
+void preProcessFile (std::string, std::string);
+void expandMacros (std::string, std::string);
+void assembleCode (std::string, std::string);
+
+
+
+/*      FUNÇÕES     */
+
+int errorCheck (int argc, char *argv[]) {
+    
+    if (argc != 4) {
+        std::cout << "Número inválido de argumentos: " << argc-1 << "\n";
+        return -1;
+    }
+    
+    std::string operation (*(argv+1));
+    
+    if (operation != "-p" && operation != "-m" && operation != "-o") {
+        std::cout << "Operação inválida: " << operation << "\n";
+        return -1;
+    }
+    
+    std::string inFileName (*(argv+2));
+    std::ifstream asmFile (inFileName);
+    
+    if (!asmFile.is_open()) {
+        std::cout << "Erro ao abrir o arquivo de entrada: " << inFileName << "\n";
+        return -1;
+    } else
+        asmFile.close();
+    
+    return 0;
+}
+
+std::string o2pre (std::string original) {
+    
+    std::string altered (original);
+    
+    altered.pop_back();
+    altered.append("pre");
+    
+    return altered;
+}
+
+std::string o2mcr (std::string original) {
+    
+    std::string altered (original);
+    
+    altered.pop_back();
+    altered.append("mcr");
+    
+    return altered;
+}
+
+void preProcessFile (std::string inFileName, std::string preFileName) {
+    
+    std::ifstream asmFile (inFileName);
+    std::ofstream preFile (preFileName);
+
+    while (!asmFile.eof()) {
+        
+        std::string line;
+        getline(asmFile, line);
+        
+        // transforma a string p caixa alta
+        std::transform (line.begin(), line.end(), line.begin(), ::toupper);
+        
+        preFile << line << "\n";
+    }
+    
+    asmFile.close();
+    preFile.close();
+    
+    return;
+}
+
+void expandMacros (std::string preFileName, std::string mcrFileName) {
+    
+    std::ifstream preFile (preFileName);
+    std::ofstream mcrFile (mcrFileName);
+
+    while (!preFile.eof()) {
+        std::string line;
+        getline(preFile, line);
+        mcrFile << line << "\n";
+    }
+    
+    preFile.close();
+    mcrFile.close();
+    
+    return;
+}
+
+void assembleCode (std::string mcrFileName, std::string outFileName) {
+    
+    std::ifstream mcrFile (mcrFileName);
+    std::ofstream outFile (outFileName);
+
+    while (!mcrFile.eof()) {
+        
+        std::string line;
+        getline(mcrFile, line);
+        // std::cout << "linha: " << line << "\n";
+        
+        std::stringstream ss (line);
+        
+        while (!ss.eof()) {
+            std::string token;
+            ss >> token;
+            // std::cout << "\ttoken: " << token << "\n";
+        }
+        
+        outFile << line << "\n";
+    }
+    
+    mcrFile.close();
+    outFile.close();
+    
+    return;
+}
--- main.cpp
+++ main.cpp
@@ -3,32 +3,33 @@
 // compilar com
 // g++ -std=c++11 main.cpp -o main.out
 
-int main () {
+// rodar com
+// ./main.out -x xxx.asm yyy.o
+
+int main (int argc, char *argv[]) {
     
-    std::ifstream asmFile ("arquivos teste - moodle/triangulo.asm");
+    if (errorCheck(argc, argv) == -1)
+        return 0;
     
-    if (asmFile.is_open()) {
+    // coloca os argumentos em strings
+    std::string operation ( *(argv+1) ),
+        inFileName ( *(argv+2) ),
+        outFileName ( *(argv+3) );
         
-        while (!asmFile.eof()) {
-            
-            std::string line;
-            getline(asmFile, line);
-            std::cout << "linha: " << line << "\n";
-            
-            std::stringstream ss (line);
-            
-            while (!ss.eof()) {
-                std::string token;
-                ss >> token;
-                std::cout << "\ttoken: " << token << "\n";
-            }
-            
-        }
+    std::string preFileName (o2pre(outFileName)),
+        mcrFileName (o2mcr(outFileName));
+    
+    // passagem de pre processamento
+    if (operation == "-p" || operation == "-m" || operation == "-o")
+        preProcessFile (inFileName, preFileName);
         
-        asmFile.close();
-        
-    } else
-        std::cout << "Erro ao abrir o arquivo." << "\n";
+    // passagem de macros
+    if (operation == "-m" || operation == "-o")
+        expandMacros (preFileName, mcrFileName);
+    
+    // passagem normal
+    if (operation == "-o")
+        assembleCode (mcrFileName, outFileName);
     
     return 0;
     
