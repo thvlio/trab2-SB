251c0290c2470e10e36212a6a24ad0c44bb72675
thvlio <thulionoslen_@hotmail.com>
2017-10-06 19:58:36 -0300

    Codigo de exemplo adicionado; um dos codigos existentes foi modificado

--- mt/pre.asm
+++ mt/pre.asm
@@ -1,10 +1,11 @@
 L1: EQU 1
-L2: EQU 0
+L2: 
+    EQU 0
 SECTION TEXT
 IF L1
 LOAD SPACE ;faz esta operação se L1 for verdadeiro
 IF L2
-INPUT SPACE ;faz esta operação se L2 for verdadeiro
+    INPUT SPACE ;faz esta operação se L2 for verdadeiro
 
 SECTION DATA
 N: SPACE
