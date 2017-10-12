09ad1bbbe483d9de7932fe15bab8e58cb9fedc67
thvlio <thulionoslen_@hotmail.com>
2017-10-12 20:34:29 -0300

    Criada função que verifica se uma string é um número (decimal ou hexa, negativo ou positivo) (para a diretiva CONST)

--- asm.h
+++ asm.h
@@ -3,11 +3,51 @@
 
 
 /*      DECLARAÇÕES DAS FUNÇÕES      */
+int constCheck (std::string&, int&);
 void assembleCode (std::string, std::string);
 
 
 
 /*      DEFINIÇÕES DAS FUNÇÕES      */
+
+/*
+constCheck: checa se a string pode ser convertida em um numero (decimal ou hexa, negativo ou positivo) sem erros
+entrada: string a ser convertida e inteiro que armazenará o resultado
+saida: inteiro que determina se houve ou não erro na conversão
+*/
+int constCheck (std::string &value, int &conv) {
+    
+    char *ptr; // ponteiro para o ultimo caracter lido por strtol
+    const char *cValue = value.c_str(); // array de caracteres equivalente à string
+    
+    // converte o array para um numero decimal e altera ptr para o ultimo caracter lido
+    conv = strtol(cValue, &ptr, 10);
+    
+    // se o numero nao eh um numero decimal valido, verifica se eh um numero em hexa valido
+    if (*ptr != '\0') {
+        
+        // se for um 'H' no final, troca por '0X' no começo, que strtol entende como hexa
+        if (value.back() == 'H') {
+            
+            value.pop_back();
+            if (value.front() == '-')
+                value = "-0X" + value.substr(1);
+            else
+                value = "0X" + value;
+                
+            const char *cValue2 = value.c_str(); // gera novamente o array de caracteres
+            conv = strtol(cValue2, &ptr, 16); // converte o array para um numero hexa
+            
+        } else
+            conv = strtol(cValue, &ptr, 16);
+        
+    }
+    
+    // se o ultimo caracter lido foi um '\0', entao a string foi convertida em um numero (decimal ou hexa)
+    return (*ptr=='\0');
+    
+}
+
 /*
 assembleCode: faz a passagem de montagem no arquivo, que inclui:
     - (todo o processo de passagem unica)
